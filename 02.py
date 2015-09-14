3dd4298ff9019acf70bdb20e28b95d073f6cdbb3
alex <alexlai@softlab.cs.tsukuba.ac.jp>
2015-09-14 18:12:00 +0800

    02 accepted

--- 02.py
+++ 02.py
@@ -1,77 +1,55 @@
-class ListNode:
-	def __init__(self,x):
-		self.val = x
-		self.next = None
+__author__ = 'alex'
 
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-#Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-#Output: 7 -> 0 -> 8
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        result = None
+        result_cursor = None
+        while not (l1 is None) or not (l2 is None):
+            if l1 is None:
+                digit1 = 0
+            else:
+                digit1 = l1.val
+            if l2 is None:
+                digit2 = 0
+            else:
+                digit2 = l2.val
+            sum = digit1 + digit2 + carry
+            carry = sum / 10
+            if result is None:
+                result = ListNode(sum % 10)
+                result_cursor = result
+            else:
+                result_cursor.next = ListNode(sum % 10)
+                result_cursor = result_cursor.next
+            if not (l1 is None):
+                l1 = l1.next
+            if not (l2 is None):
+                l2 = l2.next
 
-def buildList(nums):
-	def build(lastNode):
-		node = ListNode
-	list = None
-	for num in nums:
-		node = ListNode(num)
+        if carry != 0:
+            result_cursor.next = ListNode(carry)
+        return result
 
 node1 = ListNode(2)
-node2 = ListNode(4)
-node3 = ListNode(3)
-node1.next = node2
-node2.next = node3
+node1.next = ListNode(4)
+node1.next.next = ListNode(3)
 
-node4 = ListNode(5)
-node5 = ListNode(6)
-node6 = ListNode(4)
-node4.next = node5
-node5.next = node6
+node2 = ListNode(5)
+node2.next = ListNode(6)
+node2.next.next = ListNode(4)
+node2.next.next.next = ListNode(7)
 
-class Solution:
-    # @param {ListNode} l1
-    # @param {ListNode} l2
-    # @return {ListNode}
-    def addTwoNumbers(self, l1, l2):
-    	stack1 = []
-    	stack2 = []
-    	result = None
-    	result2 = None
-    	while l1:
-    		stack1.append(l1.val)
-    		l1 = l1.next
-    	while l2:
-    		stack2.append(l2.val)
-    		l2 = l2.next
-    	if(len(stack1) >= len(stack2)):
-    		digAdd = None
-    		for i in range(len(stack2)):
-    			dig1 = stack1.pop()
-    			dig2 = stack2.pop()
-    			#print "dig1: %d dig2: %d" %(dig1,dig2)
-    			if digAdd != None:
-    				digSum = (dig1 + dig2) % 10 + digAdd
-    			else:
-    				digSum = (dig1 + dig2) % 10
-    			digAdd = (int)((dig1 + dig2) / 10)
-    			#print "result %d" %digSum
+node3 = ListNode(0)
+solution = Solution()
+result = solution.addTwoNumbers(node1, node3)
 
-    			if result == None:
-    				result = ListNode(digSum)
-    				result2 = result
-    			else:
-    				result.next = ListNode(digSum)
-    				result = result.next
-    		else:
-    			if digAdd != 0 and digAdd != None:
-    				result.next = ListNode(digAdd)
-
-    	return result2
-    			
-
-#sdf
-
-test = Solution()
-result = test.addTwoNumbers(node1,node4)
-while result != None:    	
-	print result.val
-	result = result.next
+while not(result is None):
+    print result.val
+    result = result.next
 
