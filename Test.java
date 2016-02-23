0daa4038d7b2300f02b007046eaa10d7797a200d
alex <alexlai@softlab.cs.tsukuba.ac.jp>
2016-02-23 13:47:05 +0900

    21 accepted

--- Test.java
+++ Test.java
@@ -3,12 +3,14 @@
  */
 public class Test {
     public static void main(String[] arg) {
-        Solution26 solution26 = new Solution26();
-        int[] nums = {};
-        System.out.println(solution26.removeDuplicates(nums));
-        for (int num:nums) {
-            System.out.print(num+" ");
+        Solution21 solution21 = new Solution21();
+        ListNode l1 = null;
+        ListNode l2 = new ListNode(0);
+        l2.next = new ListNode(4);
+        ListNode head = solution21.mergeTwoLists(l1,l2);
+        while(head != null){
+            System.out.println(head.val);
+            head = head.next;
         }
     }
-
 }
